badd04f16cdbbc544983381ff3ca7bb1d4959dfc
yulvv <2961367420@qq.com>
2021-10-22 18:19:53 +0800

    project upload

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
-from .models import Inventory, Client, Order, Distribution, Evaluation, Brand
+from .models import Brand, ProductType, ProductInventory, User, Client, Logistics, ShoppingCart, Order, ProductPurchase
+from .models import Transportation, Rate, Manager, ManagerReply
 
 
 # Register your models here.
+@admin.register(Brand)
+class Brand(admin.ModelAdmin):
+    list_display = ["brand_id", "brand_name", "brand_grade"]
 
-@admin.register(Inventory)
-class Inventory(admin.ModelAdmin):
-    list_display = ["product_id", "product_name","product_quantity", "product_price", "product_size", "product_color"]
     # list_editable = ["product_price"] #可编辑单价
     # list_display_links = ["product_name"] #点击类名跳转
     # filter_vertical = ["product_name"] #垂直筛选
@@ -14,26 +15,57 @@ class Inventory(admin.ModelAdmin):
     # list_filter = ["product_name"] #右侧可以按类名筛选
     # search_fields = ["product_name"] #主界面模糊搜索
 
+@admin.register(ProductType)
+class ProductType(admin.ModelAdmin):
+    list_display = ["type_id", "type_name"]
+
+@admin.register(ProductInventory)
+class ProductInventory(admin.ModelAdmin):
+    list_display = ["product_id", "product_brand", "product_type", "product_name", "product_size", "product_quantity",
+                    "product_price", "product_color", "product_sold_quantity"]
+
+@admin.register(User)
+class User(admin.ModelAdmin):
+    list_display = ["user_id", "user_pwd", "user_nickname"]
+
 @admin.register(Client)
 class Client(admin.ModelAdmin):
-        list_display = ["client_id", "client_name", "client_sex", "client_phone_number",
-                        "client_email", "client_region", "client_address"]
+    list_display = ["client_identity_card", "client_name", "client_sex", "client_phone_number", "client_email",
+                    "client_region", "client_address", "client_user_id"]
+
+@admin.register(Logistics)
+class Logistics(admin.ModelAdmin):
+    list_display = ["logistics_id", "logistics_name", "logistics_person", "logistics_phone", "logistics_country"]
+
+
+@admin.register(ShoppingCart)
+class ShoppingCart(admin.ModelAdmin):
+    list_display = ["cart_id", "cart_product", "cart_user", "cart_product_quantity", "cart_add_time"]
 
 @admin.register(Order)
 class Order(admin.ModelAdmin):
-        list_display = ["order_id", "order_product_id", "order_brand_id", "order_transport_id", "order_evaluate_id",
-                        "order_client_id", "order_condition", "order_purchase_quantity","order_aggregate_amount"]
+    list_display = ["order_id", "order_condition", "order_aggregate_amount", "order_purchase_time"]
 
-@admin.register(Distribution)
-class Distribution(admin.ModelAdmin):
-        list_display = ["distribution_id", "distribution_company_name", "distribution_fee", "distribution_condition",
-                        "distribution_order_id"]
+@admin.register(ProductPurchase)
+class ProductPurchase(admin.ModelAdmin):
+    list_display = ["purchase_order_id", "purchase_cart", "purchase_aggregate_amount"]
 
-@admin.register(Evaluation)
-class Evaluation(admin.ModelAdmin):
-        list_display = ["evaluation_id", "evaluation_sum", "evaluation_product", "evaluation_distribution",
-                        "evaluation_description", "evaluation_order_id", "evaluation_condition"]
+@admin.register(Transportation)
+class Transportation(admin.ModelAdmin):
+    list_display = ["transportation_id", "transportation_company_name", "transportation_fee",
+                    "transportation_condition", "transportation_order"]
 
-@admin.register(Brand)
-class Evaluation(admin.ModelAdmin):
-        list_display = ["brand_id", "brand_name", "brand_grade"]
+@admin.register(Rate)
+class Rate(admin.ModelAdmin):
+    list_display = ["rate_id", "rate_total", "rate_product", "rate_transport", "rate_content", "rate_order", "rate_status",
+                    "rate_time"]
+
+@admin.register(Manager)
+class Manager(admin.ModelAdmin):
+    list_display = ["manager_id", "manager_name", "manager_pwd"]
+    
+@admin.register(ManagerReply)
+class ManagerReply(admin.ModelAdmin):
+    list_display = ["reply_id", "reply_manager_id", "reply_rate_id", "reply_time", "reply_content"]
+
+
--- inventory/models.py
+++ inventory/models.py
@@ -1,30 +1,75 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
-# 定义产品库存表单
-class Inventory(models.Model):
+# 定义入驻品牌表单
+class Brand(models.Model):
     # 继承models类
 
     class Meta:
-        verbose_name = _("库存表")
-        verbose_name_plural = _("库存表")
+        verbose_name = _("入驻品牌信息表")
+        verbose_name_plural = _("入驻品牌信息表")
 
-    # 产品号（CHAR PRIMARY KEY）√，
-    # 产品名称(CHAR NOT NULL)
-    # 产品尺码（default标码，也可以有S、M、L）√，
-    # 产品库存数量（INT NOT NULL）√×，
-    # 产品单价（INT NOT NULL）√，
-    # 产品图片（看实现难度），存url，应该可以；×
-    # 产品颜色 √，
-    # 销售数量 ×
-    # Create your models here.
+    # 品牌号
+    brand_id = models.CharField(primary_key=True, verbose_name=_("品牌号"), max_length=40, default="")
 
-    # 产品名称
-    product_name = models.CharField(max_length=40, verbose_name=_("产品名称"), null=False, blank=False)
+    # 品牌名称
+    brand_name = models.CharField(verbose_name=_("品牌名称"), max_length=40, unique=True, default="")
+
+    # 品牌等级的类型
+    BRAND_GRADE_CHOICE = (
+        ('一星品牌', _('*'))
+        , ('二星品牌', _('**'))
+        , ('三星品牌', _('***'))
+        , ('四星品牌', _('****'))
+        , ('五星品牌', _('*****'))
+    )
+
+    # 品牌等级
+    brand_grade = models.CharField(choices=BRAND_GRADE_CHOICE, verbose_name=_("品牌等级"), max_length=10, default="")
+
+    def __str__(self):
+        return str(self.brand_name)
+
+# 定义商品类别表
+class ProductType(models.Model):
+
+    class Meta:
+        verbose_name = _("商品类别表")
+        verbose_name_plural = _("商品类别表")
+
+    # 商品类别编号
+    type_id = models.CharField(verbose_name=_("商品类别编号"), primary_key=True, max_length=40, default="")
+
+    # 商品类别名称
+    type_name = models.CharField(verbose_name=_("商品类别名称"), max_length=40, null="")
+
+    def __str__(self):
+        return str(self.type_name)
+
+# 定义产品库存表单
+class ProductInventory(models.Model):
+
+    class Meta:
+        verbose_name = _("产品库存表")
+        verbose_name_plural = _("产品库存表")
 
     # 产品号
-    product_id = models.CharField(primary_key=True, verbose_name=_("产品号"),
-                                  default="",max_length=40,unique=True)
+    product_id = models.CharField(verbose_name=_("产品号"), primary_key=True, max_length=40, default="")
+
+    # 品牌名称
+    product_brand = models.ForeignKey(to=Brand, related_name="product_brand_brand", on_delete=models.CASCADE,
+                                      verbose_name=_("品牌名称"), max_length=40, default="")
+
+    # 产品类别
+    product_type = models.ForeignKey(to=ProductType, related_name="product_type_product_type", on_delete=models.CASCADE,
+                                     verbose_name=_("产品类别"), max_length=40, default="")
+
+    # 产品名称
+    product_name = models.CharField(verbose_name=_("产品名称"), max_length=40, default="")
+
+    # 产品图片
+    # product_photo = LimitSizeImageField(upload_to=user_directory_path, verbose_name=_("Proof Image"),
+    # blank=True, null=True)
 
     # 产品尺码的类型
     PRODUCT_SIZE_CHOICE = (
@@ -35,44 +80,53 @@ class Inventory(models.Model):
     )
 
     # 产品尺码
-    product_size = models.CharField(choices=PRODUCT_SIZE_CHOICE, verbose_name=_("产品尺码"), max_length=40, default="均码",
-                                    null=False, blank=False)
+    product_size = models.CharField(choices=PRODUCT_SIZE_CHOICE, verbose_name=_("产品尺码"), max_length=40, default="均码")
 
     # 产品库存数量
-    product_quantity = models.PositiveIntegerField(verbose_name=_("产品库存数量"), null=False)
+    product_quantity = models.PositiveIntegerField(verbose_name=_("产品库存数量"))
 
     # 产品单价
-    product_price = models.FloatField(verbose_name=_("产品单价"), null=False)
+    product_price = models.FloatField(verbose_name=_("产品单价"))
 
     # 产品颜色
-    product_color = models.CharField(max_length=40, verbose_name=_("产品颜色"), null=False)
+    product_color = models.CharField(verbose_name=_("产品颜色"), max_length=40, default="")
 
-    # 产品图片
-    product_image = models.CharField(max_length=40, verbose_name=_("产品图片URL"))
+    # 销售数量
+    product_sold_quantity = models.PositiveIntegerField(verbose_name=_("销售数量"))
+
+    def __str__(self):
+        return str(self.product_name) + "  "+str(self.product_color) + "  " + str(self.product_size)
+
+# 定义用户登陆表
+class User(models.Model):
+
+    class Meta:
+        verbose_name = _("用户登陆表")
+        verbose_name_plural = _("用户登陆表")
+
+    # 用户号
+    user_id = models.CharField(primary_key=True, verbose_name=_("用户号"), max_length=40, unique=True, default="")
+
+    # 用户密码
+    user_pwd = models.CharField(verbose_name=_("用户密码"), max_length=40, default="")
+
+    # 用户昵称
+    user_nickname = models.CharField(verbose_name=_("用户昵称"), max_length=40, unique=True, default="")
+
+    def __str__(self):
+        return str(self.user_id)
 
 # 定义客户表单
 class Client(models.Model):
-    # 继承models类
-
     class Meta:
-        verbose_name = _("客户单")
-        verbose_name_plural = _("客户单")
+       verbose_name = _("客户信息表")
+       verbose_name_plural = _("客户信息表")
 
-    # 客户管理（客户号，客户名称，客户手机号，客户区域，客户地址）
-    # 客户号（CHAR，PRIMARY KEY),√
-    # 客户性别(CHAR)，√
-    # 客户姓名(CHAR NOT NULL)，√
-    # 客户手机号(CHAR NOT NULL)，√
-    # 客户邮箱(CHAR)，√
-    # 客户区域(CHAR NOT NULL)，（如表中所示，直接分为几个地区(例如中国的东北、华东华南，或者七大洲)，分别对应不同的物流公司） √
-    # 客户地址(CHAR NOT NULL)；√
+    # 身份证号
+    client_identity_card = models.CharField(primary_key=True, verbose_name=_("身份证号"), max_length=40, default="")
 
-    # 客户名
-    client_name = models.CharField(max_length=40, verbose_name=_("客户名"), null=False, blank=False)
-
-    # 客户号
-    client_id = models.CharField(primary_key=True, verbose_name=_("客户号"),
-                                 default="", max_length=40, unique=True)
+    # 客户姓名
+    client_name = models.CharField(verbose_name=_("客户名"), max_length=40, default="")
 
     # 客户性别的类型
     CLIENT_SEX_CHOICE = (
@@ -81,15 +135,13 @@ class Client(models.Model):
     )
 
     # 客户性别
-    client_sex = models.CharField(choices=CLIENT_SEX_CHOICE, max_length=127, default="",
-                                  verbose_name=_("客户性别"))
+    client_sex = models.CharField(choices=CLIENT_SEX_CHOICE, verbose_name=_("客户性别"), max_length=10, default="")
 
     # 客户手机号
-    client_phone_number = models.CharField(max_length=40, verbose_name=_("客户手机号"),
-                                           null=False, blank=False)
+    client_phone_number = models.CharField(verbose_name=_("客户手机号"), max_length=40, unique=True, default="")
 
     # 客户邮箱
-    client_email = models.CharField(max_length=40, verbose_name=_("客户邮箱"), default="")
+    client_email = models.CharField(verbose_name=_("客户邮箱"), max_length=40, default="")
 
     # 客户区域的类型
     CLIENT_REGION_CHOICE = (
@@ -101,52 +153,94 @@ class Client(models.Model):
         , ('委内瑞拉', _('VE'))
     )
 
-
     # 客户区域
-    client_region = models.CharField(choices=CLIENT_REGION_CHOICE, max_length=127, default="",
-                                     verbose_name=_("客户区域"))
+    client_region = models.CharField(choices=CLIENT_REGION_CHOICE, verbose_name=_("客户区域"), max_length=40,
+                                     default="")
 
     # 客户地址
-    client_address = models.CharField(max_length=40, verbose_name=_("客户地址"), null=False, blank=False)
+    client_address = models.CharField(verbose_name=_("客户地址"), max_length=40, default="")
 
-# 定义订单表单
-class Order(models.Model):
+    # 用户号
+    client_user_id = models.ForeignKey(to=User, related_name="client_user_id_user", on_delete=models.CASCADE,
+                                       verbose_name=_("用户号"), max_length=40, default="")
+
+    def __str__(self):
+        return str(self.client_name + " " + str(self.client_phone_number) + " " + str(self.client_address))
+
+
+# 定义物流公司信息表
+class Logistics(models.Model):
+
+    class Meta:
+        verbose_name = _("物流公司信息表")
+        verbose_name_plural = _("物流公司信息表")
+
+    # 物流公司号
+    logistics_id = models.CharField(primary_key=True, verbose_name=_("物流公司号"), max_length=40, default="")
+
+    # 物流公司名
+    logistics_name = models.CharField(verbose_name=_("物流公司名"), max_length=40, default="")
+
+    # 联系人
+    logistics_person = models.CharField(verbose_name=_("联系人"), max_length=40, default="")
+
+    # 电话号码
+    logistics_phone = models.CharField(verbose_name=_("电话号码"), max_length=40, default="")
+
+    # 客户区域的类型
+    REGION_CHOICE = (
+        ('奥地利', _('AT'))
+        , ('中国', _('CN'))
+        , ('英国', _('GB'))
+        , ('新加坡', _('SG'))
+        , ('美国', _('US'))
+        , ('委内瑞拉', _('VE'))
+    )
+    # 所在国家
+    logistics_country = models.CharField(choices=REGION_CHOICE, verbose_name=_("所在国家"), max_length=40, default="")
+
+    def __str__(self):
+        return str(self.logistics_country) + "  " + str(self.logistics_name)
+
+# 定义购物车情况表单
+class ShoppingCart(models.Model):
     # 继承models类
 
     class Meta:
-        verbose_name = _("订单表")
-        verbose_name_plural = _("订单表")
+        verbose_name = _("购物车情况表")
+        verbose_name_plural = _("购物车情况表")
 
-    # 订单号（CHAR，PRIMARY KEY)，√
-    # 产品号（CHAR, Foreign KEY），√
-    # 品牌号（CHAR, Foreign KEY），√
-    # 运输号（CHAR, Foreign KEY），√
-    # 评价号（CHAR, Foreign KEY），√
-    # 客户号（CHAR, Foreign KEY），√
-    # 订单状态（成功，　关闭，　进行中）（感觉这个跟物流约束有配合关系）√
-    # 产品数量（INT NOT NULL），（限制其小于库存）√
-    # 消费总金额（INT NOT NULL），（其等于单价和数量之积）√
-    # 下单时间（自动读取当前时间）；×
+    unique_together = (("cart_id", "cart_product"),)
+
+    # 购物车号
+    cart_id = models.CharField(verbose_name=_("购物车号"), max_length=40, default="")
+
+    # 用户信息
+    cart_user = models.ForeignKey(to=Client, related_name="cart_user_client", verbose_name=_("用户名称"), max_length=40,
+                                  on_delete=models.CASCADE, default="")
+
+    # 产品名称
+    cart_product = models.ForeignKey(to=ProductInventory, related_name="product_name_product_inventory",
+                                     verbose_name=_("商品名称"), on_delete=models.CASCADE, max_length=40, default="")
+
+    # 产品数量
+    cart_product_quantity = models.PositiveIntegerField(verbose_name=_("产品数量"))
+
+    # 添加时间
+    cart_add_time = models.DateField(auto_now=True, verbose_name=_("添加时间"))
+
+    def __str__(self):
+        return str(self.cart_user) + "  " + str(self.cart_product) + "  " + str(self.cart_product_quantity)
+
+# 定义订单信息表单
+class Order(models.Model):
+
+    class Meta:
+        verbose_name = _("订单信息表")
+        verbose_name_plural = _("订单信息表")
 
     # 订单号
-    order_id = models.CharField(primary_key=True, verbose_name=_("订单号"),
-                                default="", max_length=40)
-
-    # 产品号
-    order_product_id = models.CharField(verbose_name=_("产品号"), max_length=40)
-
-    # 品牌号
-    order_brand_id = models.CharField(verbose_name=_("品牌号"), max_length=40)
-
-    # 运输号
-    order_transport_id = models.CharField(verbose_name=_("运输号"), max_length=40)
-
-    # 评价号
-    order_evaluate_id = models.CharField(verbose_name=_("评价号"), max_length=40)
-
-    # 客户号
-    order_client_id = models.CharField(verbose_name=_("客户号"), max_length=40)
-
+    order_id = models.CharField(primary_key=True, verbose_name=_("订单号"), max_length=40, default="")
 
     # 订单状态的类型
 
@@ -157,46 +251,35 @@ class Order(models.Model):
     )
 
     # 订单状态
-    order_condition = models.CharField(choices=ORDER_CONDITION_CHOICE, max_length=127, default="",
-                                       verbose_name=_("订单状态"))
-
-    # 购买数量
-    order_purchase_quantity = models.PositiveIntegerField(verbose_name=_("购买数量"), null=False)
+    order_condition = models.CharField(choices=ORDER_CONDITION_CHOICE, verbose_name=_("订单状态"), max_length=40, default="")
 
     # 消费总金额
-    order_aggregate_amount = models.FloatField(verbose_name=_("消费总金额"), null=False)
+    order_aggregate_amount = models.FloatField(verbose_name=_("消费总金额"))
 
     # 下单时间
+    order_purchase_time = models.DateTimeField(auto_now=True, verbose_name=_("下单时间"))
+
+    def __str__(self):
+        return str(self.order_id)
 
 # 定义物流表单
-class Distribution(models.Model):
-    # 继承models类
-
+class Transportation(models.Model):
     class Meta:
-        verbose_name = _("物流表")
-        verbose_name_plural = _("物流表")
-
-    # 物流号（CHAR，PRIMARY KEY NOT NULL)， √
-    # 物流公司名称(CHAR NOT NULL)， √
-    # 物流区域(CHAR NOT NULL)，×
-    # 邮费 √
-    # 物流情况(CHAR NOT NULL)；（限制几种情况，如等待发货、运输中、待签收、已签收） √
-    # 订单号（外键） √
+        verbose_name = _("物流信息表")
+        verbose_name_plural = _("物流信息表")
 
     # 物流id
-    distribution_id = models.CharField(primary_key=True, verbose_name=_("物流号"),
-                                       default="", max_length=40)
+    transportation_id = models.CharField(primary_key=True, verbose_name=_("物流号"), max_length=40, default="")
 
     # 物流公司名称
-    distribution_company_name = models.CharField(max_length=40, verbose_name=_("物流公司名称"),
-                                                 null=False, blank=False)
-
-    # 物流区域
+    transportation_company_name = models.ForeignKey(to=Logistics, related_name="transportation_company_name_Logistics",
+                                                    verbose_name=_("物流公司名称"), on_delete=models.CASCADE, max_length=40,
+                                                    default="")
 
     # 邮费
-    distribution_fee = models.FloatField(verbose_name=_("邮费"), null=False)
+    transportation_fee = models.FloatField(verbose_name=_("邮费"))
 
-    DISTRIBUTION_CONDITION_CHOICE = (
+    TRANSPORTATION_CONDITION_CHOICE = (
         ('waiting for delivery', _('等待发货'))
         , ('transporting', _('运输中'))
         , ('to be signed', _('待签收'))
@@ -204,37 +287,42 @@ class Distribution(models.Model):
     )
 
     # 物流情况
-    distribution_condition = models.CharField(choices=DISTRIBUTION_CONDITION_CHOICE, max_length=127,
-                                              default="", verbose_name=_("物流情况"))
+    transportation_condition = models.CharField(choices=TRANSPORTATION_CONDITION_CHOICE, verbose_name=_("物流情况"),
+                                                max_length=40, default="")
 
     # 订单id
-    distribution_order_id = models.CharField(verbose_name=_("订单号"),default="", max_length=40, null=False)
+    transportation_order = models.ForeignKey(to=Order, on_delete=models.CASCADE, verbose_name=_("订单号"),
+                                             related_name="transportation_order_order", max_length=40, default="")
 
-# 定义评价表单
-class Evaluation(models.Model):
-    # 继承models类
-
+# 定义产品购买信息表
+class ProductPurchase(models.Model):
     class Meta:
-        verbose_name = _("评价表")
-        verbose_name_plural = _("评价表")
+        verbose_name = _("产品购买信息表")
+        verbose_name_plural = _("产品购买信息表")
 
-    # 评价号（CHAR，PRIMARY KEY)，
-    # 总评价星数（INT，1-5），
-    # 产品评价星数（INT，1-5），
-    # 物流评价星数（INT，1-5），
-    # 评价文案(CHAR)，
-    # 评价图片（看实现的难度），×
-    # 订单号（外键）
-    # 产品号（外键）×
-    # 客户号（外键）×
-    # 评价状态（未到货不可评价，已到货未评价，已评价）；
+    unique_together = (("purchase_order_id", "purchase_cart"),)
+
+    # 订单号
+    purchase_order_id = models.CharField(verbose_name=_("订单号"), max_length=40, default="")
+
+    # 订单信息
+    purchase_cart = models.ForeignKey(to=ShoppingCart, related_name="order_cart_shopping_cart", verbose_name=_("具体信息"),
+                                      on_delete=models.CASCADE, max_length=40, default="")
+
+    # 产品总价
+    purchase_aggregate_amount = models.FloatField(verbose_name=_("产品总价"))
+
+# 定义订单评价表单
+class Rate(models.Model):
+    class Meta:
+        verbose_name = _("订单评价表")
+        verbose_name_plural = _("订单评价表")
 
     # 评价号
-    evaluation_id = models.CharField(primary_key=True, verbose_name=_("评价号"),
-                                     default="", max_length=40)
+    rate_id = models.CharField(primary_key=True, verbose_name=_("评价号"), max_length=40, default="")
 
     # 评价星数的类型
-    EVALUATION_CHOICE = (
+    RATE_CHOICE = (
         ('一星评价', _('*'))
         , ('二星评价', _('**'))
         , ('三星评价', _('***'))
@@ -243,49 +331,76 @@ class Evaluation(models.Model):
     )
 
     # 总评价星数
-    evaluation_sum = models.CharField(choices=EVALUATION_CHOICE, max_length=127,
-                                      default="", verbose_name=_("总评价星数"))
-
+    rate_total = models.CharField(choices=RATE_CHOICE, verbose_name=_("总评价星数"), max_length=40, default="*****")
 
     # 产品评价星数
-    evaluation_product = models.CharField(choices=EVALUATION_CHOICE, max_length=127,
-                                          default="", verbose_name=_("产品评价星数"))
+    rate_product = models.CharField(choices=RATE_CHOICE, verbose_name=_("产品评价星数"), max_length=40, default="*****")
 
     # 物流评价星数
-    evaluation_distribution = models.CharField(choices=EVALUATION_CHOICE, max_length=127,
-                                               default="", verbose_name=_("物流评价星数"))
+    rate_transport = models.CharField(choices=RATE_CHOICE, verbose_name=_("物流评价星数"), max_length=40, default="*****")
 
     # 评价文案
-    evaluation_description = models.CharField(max_length=40, verbose_name=_("评价文案"), default="")
+    rate_content = models.CharField(verbose_name=_("评价文案"), max_length=200, default="")
 
     # 订单号
-    evaluation_order_id = models.CharField(max_length=40, verbose_name=_("订单号"), default="")
+    rate_order = models.ForeignKey(to=Order, related_name="rate_order_order", verbose_name=_("订单号"), max_length=40,
+                                   on_delete=models.CASCADE, default="")
 
-    EVALUATION_CONDITION_CHOICE = (
+    RATE_CONDITION_CHOICE = (
         ('cannot evaluate', _('未到货不可评价'))
         , ('no evaluate', _('已到货未评价'))
         , ('have evaluated', _('已评价'))
     )
 
-    evaluation_condition = models.CharField(choices=EVALUATION_CONDITION_CHOICE, max_length=127,
-                                            default="", verbose_name=_("评价状态"))
-# 定义入驻品牌表单
-class Brand(models.Model):
-    # 继承models类
+    # 评价状态
+    rate_status = models.CharField(choices=RATE_CONDITION_CHOICE, verbose_name=_("评价状态"), max_length=40, default="")
+
+    # 评价时间
+    rate_time = models.DateTimeField(auto_now_add=True, verbose_name=_("评价时间"))
+
+    def __str__(self):
+        return str(self.rate_id)
+
+# 定义系统管理员表
+class Manager(models.Model):
 
     class Meta:
-        verbose_name = _("入驻品牌表")
-        verbose_name_plural = _("入驻品牌表")
+        verbose_name = _("系统管理员表")
+        verbose_name_plural = _("系统管理员表")
 
-    # 品牌号（CHAR，PRIMARY KEY)，
-    # 品牌名称（CHAR，UNIQUE NOT NULL)，
-    # char(40)
-    # char(40)（CHAR，NOT NULL);
+    # 管理员编号
+    manager_id = models.CharField(primary_key=True, verbose_name=_("管理员编号"), max_length=40, default="")
 
-    # 品牌号
-    brand_id = models.CharField(primary_key=True, verbose_name=_("品牌号"), default="", max_length=40)
+    # 管理员姓
+    manager_name = models.CharField(verbose_name=_("管理员姓名"), max_length=40, default="")
 
-    brand_name = models.CharField(verbose_name=_("品牌名称"), default="", max_length=40, null=False)
+    # 管理员密码
+    manager_pwd = models.CharField(verbose_name=_("管理员密码"), max_length=40, default="")
 
-    brand_grade = models.CharField(verbose_name=_("品牌等级"), default="", max_length=40,
-                                   null=False)
+    def __str__(self):
+        return str(self.manager_id) + "  " + str(self.manager_name)
+
+# 定义管理员回复留言表
+class ManagerReply(models.Model):
+
+    class Meta:
+        verbose_name = _("管理员回复")
+        verbose_name_plural = _("管理员回复")
+
+    # 回复编号
+    reply_id = models.CharField(primary_key=True, verbose_name=_("回复编号"), max_length=20, default="")
+
+    # 管理员编号
+    reply_manager_id = models.ForeignKey(to=Manager, related_name="reply_manager_id_manager", on_delete=models.CASCADE,
+                                         verbose_name=_("管理员编号"), max_length=40, default="")
+
+    # 评价号
+    reply_rate_id = models.ForeignKey(to=Rate, on_delete=models.CASCADE, verbose_name=_("评价号"),
+                                      related_name="reply_rate_id_rate", max_length=40, default="")
+
+    # 回复时间
+    reply_time = models.DateTimeField(auto_now_add=True, verbose_name=_("回复时间"))
+
+
+    # 回复内容
+    reply_content = models.CharField(verbose_name=_("回复内容"), max_length=200, default="")
